194748a7553d6bb2d06e1fd134a399640724bd00
sebastianriosanchez68@gmail.com <sebastianriosanchez68@gmail.com>
2019-03-30 11:05:21 -0400

    Se creo el Web Api

---
+++ Parcial/API PARCIAL/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace API_PARCIAL.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Parcial/API PARCIAL/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace API_PARCIAL.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
