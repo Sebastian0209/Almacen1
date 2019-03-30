96046fabd9cb29b57d7add0188fc18d7e30a4a8f
sebastianriosanchez68@gmail.com <sebastianriosanchez68@gmail.com>
2019-03-30 10:59:16 -0400

    COMMIT CON MODELO MVC

---
+++ Parcial/MVC Parcial/Models/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Parcial.Models
+{
+    public enum TipoEstado
+    {
+        Activo,
+        inactivo
+    }
+    public enum TipoUnidad
+    {
+        Botella,
+        Metro,
+        Litro,
+        Caja
+ 
+
+    } 
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        [Display(Name = "Product Name")]
+        public string Nombre { get; set; }
+
+
+        public decimal Precio { get; set; }
+        [Required]
+        public int Cantidad { get; set; }
+        [Required]
+        public TipoUnidad Unidad { get; set; }
+        [Required]
+        public TipoEstado Estado { get; set; }
+        
+    }
+}
